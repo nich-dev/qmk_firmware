29f4aaaeff41b17ad30acf01ae5522c948e8c16d
Nicholas Holley <npholley@gmail.com>
2020-03-19 14:15:32 -0400

    initial build with color

--- keyboards/massdrop/ctrl/keymaps/npholley/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/npholley/keymap.c
@@ -1,6 +1,17 @@
 #include QMK_KEYBOARD_H
 
-enum ctrl_keycodes {
+uint8_t rgb_matrix_effects[] = {
+    RGB_MATRIX_GRADIENT_UP_DOWN,
+    RGB_MATRIX_SOLID_COLOR,
+    RGB_MATRIX_BAND_SPIRAL_SAT,
+    RGB_MATRIX_NONE,
+    RGB_MATRIX_JELLYBEAN_RAINDROPS
+};
+
+uint8_t current_rgb_matrix_effect_index = 0;
+
+
+enum alt_keycodes {
     U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
     U_T_AGCR,              //USB Toggle Automatic GCR control
     DBG_TOG,               //DEBUG Toggle On / Off
@@ -8,6 +19,7 @@ enum ctrl_keycodes {
     DBG_KBD,               //DEBUG Toggle Keyboard Prints
     DBG_MOU,               //DEBUG Toggle Mouse Prints
     MD_BOOT,               //Restart into bootloader after hold timeout
+    RGB_NXT,               //Custom index for RGB mode
 };
 
 keymap_config_t keymap_config;
@@ -19,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP, \
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_LCTL, KC_LGUI, KC_LALT,             LT(1, KC_SPC),                            KC_RALT, MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [1] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            KC_MUTE, _______, _______, \
@@ -41,8 +53,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 };
 
+void set_matrix_mode(void) {
+    rgb_matrix_mode(rgb_matrix_effects[current_rgb_matrix_effect_index]);
+};
+
+// Bump and change RGB mode in a limited subset
+void increment_current_rgb_matrix_effect(void) {
+    current_rgb_matrix_effect_index++;
+    uint8_t effect_count = sizeof(rgb_matrix_effects) / sizeof(uint8_t);
+    if (current_rgb_matrix_effect_index >= effect_count) {
+        current_rgb_matrix_effect_index = 0;
+    }
+    set_matrix_mode();
+};
+
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
+    set_matrix_mode();
 };
 
 // Runs constantly in the background, in a loop.
@@ -52,6 +79,7 @@ void matrix_scan_user(void) {
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
 #define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
 #define MODS_ALT  (get_mods() & MOD_BIT(KC_LALT) || get_mods() & MOD_BIT(KC_RALT))
+#define MODS_SHIFT_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
@@ -94,6 +122,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 if (timer_elapsed32(key_timer) >= 500) {
                     reset_keyboard();
                 }
+            }
+            return false;
+        case RGB_NXT:
+            if (record->event.pressed) {
+                increment_current_rgb_matrix_effect();
             }
             return false;
         case RGB_TOG:
