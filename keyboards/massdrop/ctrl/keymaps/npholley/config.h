91ee19ae4b6afb43a23f9aaf34d297b4d5de3ef9
Nicholas Holley <npholley@gmail.com>
2020-06-11 11:09:27 -0400

    fix compile errors

--- keyboards/massdrop/ctrl/keymaps/npholley/config.h
+++ keyboards/massdrop/ctrl/keymaps/npholley/config.h
@@ -1,6 +1,6 @@
 // Startup version of matrix hue for some vaporwave
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_BAND_SPIRAL_SAT
-#define RGB_MATRIX_STARTUP_HUE 120  // Sets the default hue value, if none has been set
+#define RGB_MATRIX_STARTUP_HUE 10  // Sets the default hue value, if none has been set
 #define RGB_MATRIX_STARTUP_SAT 200 // Sets the default saturation value, if none has been set
-#define RGB_MATRIX_STARTUP_VAL 180 // Sets the default brightness value, if none has been set
-#define RGB_MATRIX_STARTUP_SPD 50 // Sets the default animation speed, if none has been set
+#define RGB_MATRIX_STARTUP_VAL 120 // Sets the default brightness value, if none has been set
+#define RGB_MATRIX_STARTUP_SPD 20 // Sets the default animation speed, if none has been set
